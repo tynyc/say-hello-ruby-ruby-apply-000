6442e142aa16d8b0d27104147f05362d10c18aaf
Timothy Yap <tim@timaaronyap.com>
2020-07-06 14:51:39 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -4,7 +4,7 @@ describe "say_hello" do
 
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Gabriela!")
-    say_hello("Gabriela")
+    say_hello("Gabriela!")
   end
 
   it 'defaults to Ruby Programmer when no name is passed in' do
